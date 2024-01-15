3ce8b406d4ed71d4845f4003cede703ab3d19f0b
54m4n <pflum.tamas@gmail.com>
2024-01-15 05:43:15 -0500

    0

---
+++ 2012-05/futar.py
@@ -0,0 +1,21 @@
+import os
+
+
+# -- 1 --
+
+path=os.path.dirname(__file__)
+f=open(f'{path}{os.sep}src{os.sep}tavok.txt')
+
+rows=[]
+
+for line in f:
+    rows.append(line.split())
+
+
+# -- 2 --
+print("-- 2 --")
+print(f'elso ut km: {rows[0][2]} km')
+
+# -- 3 --
+print("-- 3 --")
+print(f'utso ut km: {rows[len(rows)-1][2]} km')
