3c086711aec4d49777557f7b69ba25dab53888ea
54m4n <pflum.tamas@gmail.com>
2024-01-15 07:08:27 -0500

    0

--- 2012-05/futar.py
+++ 2012-05/futar.py
@@ -18,14 +18,49 @@ print(f'elso ut km: {rows[0][2]} km')
 # -- 3 --
 print("-- 3 --")
 
-print("afaszom geci")
+i=0
+km=0
+
+for i in range(len(rows)):
+    km=int(rows[i][2])
+    if(int(rows[i][0])>int(rows[i+1][0])):
+        break
+
+print(f'a het utso napja: {km} km')
 
 
 
 
+# -- 4 --
+print(" -- 4 -- ")
 
 
+hetnapok=[]
+hetszama=1
+for i in range(7):
+    hetnapok.append(i+1)
 
-print(f'utso ut km: {rows[len(rows)-1][2]} km')
+tmp_hetnapok=hetnapok.copy()
 
+for i in range(len(rows)):
+    
+    try:
+        hetnapok.remove(int(rows[i][0]))
+    except:
+        IndexError
+    
+    try:
+        if int(rows[i][0])>int(rows[i+1][0]):
+            print(f'a(z) {hetszama}. heten a futar az alabbi napokon nem dolgozott: {hetnapok}')
+            hetnapok=tmp_hetnapok.copy()
+            hetszama=hetszama+1
+            
+    except:
+        IndexError
+        if IndexError:
 
+            hetnapok=tmp_hetnapok.copy()
+            hetnapok.remove(int(rows[i][0]))
+            print(f'a(z) {hetszama}. heten a futar az alabbi napokon nem dolgozott: {hetnapok}')
+            
+            
