08bb534853ba44f367c2c6222f8de74a35fa1709
54m4n <pflum.tamas@gmail.com>
2024-01-26 07:12:25 -0500

    0

--- 2014-05/ip-cimek.py
+++ 2014-05/ip-cimek.py
@@ -1,5 +1,8 @@
 import os
 
+#--1--
+print('--1--')
+
 path=os.path.dirname(__file__)
 f=open(f'{path}{os.sep}src{os.sep}ip.txt','r')
 
@@ -10,4 +13,11 @@ ip=[]
 for i in f:
     ip.append(i.split())
 
-print(ip[0])
+
+#--2--
+print('--2--')
+print(f'{len(ip)}. sor van az allomanyban')
+
+#--3--
+print('--3--')
+
