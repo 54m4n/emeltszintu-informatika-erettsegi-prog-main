024e413e6612c6e4ed6aff34a789c82432a94f3c
54m4n <pflum.tamas@gmail.com>
2025-01-15 06:51:44 +0100

    0

--- 2015-10/fejvagyiras.py
+++ 2015-10/fejvagyiras.py
@@ -1,1 +1,29 @@
-print("ok")
+import random
+import os
+
+
+#--1--
+
+def fejvagyiras():
+    x=random.randrange(0,1)
+    return x
+
+#--2--
+#n=int(input("kerem a tippet (fej=0, iras=1)"))
+n=0
+
+if n==0:
+    tipp="fej"
+else:
+    tipp="iras"
+
+if fejvagyiras()==0:
+    sorsolas="fej"
+else:
+    sorsolas="iras"
+
+if tipp==sorsolas:
+    print(f'JO! tipp: {tipp} sorsolas: {sorsolas}')
+else:
+    print(f'NEMJO! tipp: {tipp} sorsolas: {sorsolas}')
+
