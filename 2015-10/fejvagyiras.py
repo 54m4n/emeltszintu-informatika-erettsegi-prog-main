9bcb78e45f61bb72f52c76c866a080b7de931446
54m4n <pflum.tamas@gmail.com>
2025-01-18 12:28:37 +0100

    0

--- 2015-10/fejvagyiras.py
+++ 2015-10/fejvagyiras.py
@@ -10,6 +10,7 @@ def fejvagyiras():
     return x
 
 #--2--
+print("--2--")
 #n=int(input("kerem a tippet (fej=0, iras=1)"))
 n=0
 
@@ -27,4 +28,49 @@ if tipp==sorsolas:
     print(f'JO! tipp: {tipp} sorsolas: {sorsolas}')
 else:
     print(f'NEMJO! tipp: {tipp} sorsolas: {sorsolas}')
+
+#--3-- 
+#a memoriaban torteno egyideju eltarolas nelkul kene megoldasni, csak leszarom
+print("--3--")
+
+
+path=os.path.dirname(__file__)
+
+f=open(f'{path}{os.sep}src{os.sep}kiserlet.txt')
+l=f.read().split()
+f.close()
+
+input=[]
+
+for line in l:
+    input.append(line)
+
+print(f'{len(input)} dobasbol allt a kiserlet.')
+
+#--4--
+fej=0
+for i in range(len(input)):
+    if input[i]=="F":
+        fej=fej+1
+
+print(f'ossz dobas: {len(input)}, ebbol FEJ: {fej}')
+print(f'relativ gyakorisaga a FEJ-nek: {round(fej/int(len(input))*100,2)}%')
+
+#--5--
+print("--5--")
+
+ketfej=0
+
+for i in range(len(input)):
+    try:
+        if (input[i]=="F" and input[i+1]=="F") and (input[i-1]!="F" and input[i+2]!="F"):
+            ketfej=ketfej+1
+    except:
+        pass
+
+print(f'{ketfej} alkalommal kovette egymast pontosan 2 FEJ')
+
+
+#--6--
+print("--6--")
 
