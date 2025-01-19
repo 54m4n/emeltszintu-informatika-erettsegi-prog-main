e598129164ded9b0031b69bf66fa59886f975154
54m4n <pflum.tamas@gmail.com>
2025-01-19 22:29:58 +0100

    0

--- 2016-10/telefonos-ugyfelszolgalat.py
+++ 2016-10/telefonos-ugyfelszolgalat.py
@@ -49,3 +49,31 @@ for i in range(len(input)):
         max=stop-start
 
 print(f'leghosszabb hivas masodpercben: {max}')
+
+#--5--
+print('--5--')
+ido=[10,11,12]
+idosec=mpbe(ido[0],ido[1],ido[2])
+
+megvan=False
+i=0
+
+varakozok=0
+akthivonum=0
+
+while i<len(input):
+    h1=int(input[i][0])
+    m1=int(input[i][1])
+    s1=int(input[i][2])
+    h2=int(input[i][3])
+    m2=int(input[i][4])
+    s2=int(input[i][5])
+    if megvan!=True and (mpbe(h1,m1,s1)<idosec and mpbe(h2,m2,s2)>idosec):
+        varakozok=varakozok+1
+    
+    if (mpbe(h1,m1,s1)>=idosec or mpbe(h2,m2,s2)>=idosec) and megvan!=True:
+        megvan=True
+        akthivonum=i+1
+    i=i+1
+
+print(i,akthivonum,varakozok) 
