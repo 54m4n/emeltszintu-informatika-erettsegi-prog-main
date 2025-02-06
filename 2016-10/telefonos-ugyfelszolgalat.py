998c078b688ab41368fe0e63c58efbbca661ef9f
54m4n <pflum.tamas@gmail.com>
2025-02-06 14:52:47 +0100

    0

--- 2016-10/telefonos-ugyfelszolgalat.py
+++ 2016-10/telefonos-ugyfelszolgalat.py
@@ -8,6 +8,7 @@ def mpbe(h,m,s):
     sec=h*3600+m*60+s
     return sec
 
+
 #--2--
 path=os.path.dirname(__file__)
 f=open(f'{path}{os.sep}src{os.sep}hivas.txt')
@@ -74,18 +75,39 @@ print(f'idopont: {idopont[0]}:{idopont[1]}:{idopont[2]}, beszelo a(z) {beszelok[
 #--6--
 print('--6--')
 
+def mpbol(mp):
+    h=int(mp/3600)
+    mp=mp-(h*3600)
+    m=int(mp/60)
+    mp=mp-(m*60)
+    s=mp
+    mplista=[h,m,s]
+    return mplista
+
+
+
 mpidok=[]
-tmp=0
 
 for i in range(len(input)):
     hkezdet=(mpbe(input[i][0],input[i][1],input[i][2]))
     hvege=(mpbe(input[i][3],input[i][4],input[i][5]))
-    if (hvege>meleje and hvege<mvege) or (hvege>mvege and hkezdet<mvege):
-        mpidok.append([hkezdet,hvege])
-        tmp=i
+    mpidok.append([hkezdet,hvege])
 
 print(f'meleje: {meleje}, mvege: {mvege}')
+print(f'meleje: {meleje}, mvege: {mvege}')
 
-print(tmp,mpidok[len(mpidok)-1])
 
-print(mpidok)
+hivaseleje=mpidok[0][0]
+hivasvege=mpidok[0][1]
+
+
+for i in range(len(mpidok)):
+    print(i,mpbol(mpidok[i][0]),mpbol(mpidok[i][1]))
+
+
+for i in range(len(mpidok)):    
+    if hivaseleje<mvege and mpidok[i][0]>=hivaseleje:
+        hivaseleje=mpidok[i][0]
+        hivasvege=mpidok[i][1]
+    
+print(hivaseleje,hivasvege)
