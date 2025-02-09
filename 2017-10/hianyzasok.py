cbf0f723139b95b389c228f06157ff86d5145173
54m4n <pflum.tamas@gmail.com>
2025-02-09 12:19:56 +0100

    0

--- 2017-10/hianyzasok.py
+++ 2017-10/hianyzasok.py
@@ -55,11 +55,33 @@ print(f'igazolt: {igazolt} igazolatlan: {igazolatlan}')
 #--4--
 print('--4--')
 
-def hetnapja(honap,nap):
-    napok=["hetfo","kedd","szerda","csutortok","pentek","szombat","vasarnap"]
-    napszam=[0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 335]
-    print(napok[0])
-    return
+def hetnapja(honap, nap):
+    napok=["hetfo", "kedd", "szerda", "csutortok", "pentek", "szombat", "vasarnap"]
+    hnapszam=[0,31,59,90,120,151,181,212,243,273,304,335]
+    napsorszam=hnapszam[honap-1]+nap
+    return napok[(napsorszam-1)%7]
 
-hetnapja(1,2)
+#--5--
+print('--5--')
+print(hetnapja(1,1))
 
+#--6--
+print('--6--')
+#mivel arrol kurvara nincs szo h igazolt v igazolatlan hianyzast keresunk, igy kiiratom mindet a faszba
+
+napnev="szerda"
+orasrsz=3
+h=0
+
+for i in range(len(hiany)):
+    hnap=int(hiany[i][0])
+    nap=int(hiany[i][1])
+    napneve=hetnapja(hnap,nap)
+    if napneve==napnev:
+        akthianyzasok=str(str(hiany[i][2]).split(' ')[2]).split("'")[0]
+        aktorastatusz=akthianyzasok[orasrsz-1:orasrsz:1]
+        if aktorastatusz=="I" or aktorastatusz=="X":
+            h=h+1
+
+print(f'az alabbi napokon: {napnev} az alabbi sorszamu oran: {orasrsz}, osszesen {h} hianyzas volt')
+    
