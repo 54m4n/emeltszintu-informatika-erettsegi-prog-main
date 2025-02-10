52757ba02615cb72b7bdeaefa079a875849930e3
54m4n <pflum.tamas@gmail.com>
2025-02-10 18:13:27 +0100

    0

--- 2019-10/eutazas.py
+++ 2019-10/eutazas.py
@@ -16,19 +16,4 @@ for l in f:
 f.close()
 
 print(utasadat[0])
-print("--------")
 
-u=[]
-utak=[]
-
-for i in range(len(utasadat)):
-    aktindex=int(utasadat[i][0])
-    j=i
-    while aktindex==int(utasadat[j][0]) and j<=len(utasadat):
-        u.append(utasadat[j])
-        j=j+1
-    utak.insert(aktindex,u)
-    u=[]
-    i=j
-
-print(utak[0])
