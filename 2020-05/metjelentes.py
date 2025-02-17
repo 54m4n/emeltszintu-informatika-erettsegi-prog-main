8c68ecf9cecff258229acad992f054b688700ead
54m4n <pflum.tamas@gmail.com>
2025-02-17 18:02:53 +0100

    0

---
+++ 2020-05/metjelentes.py
@@ -0,0 +1,72 @@
+import os
+import platform
+
+if platform.system()=="Windows":
+    os.system('cls')
+else:
+    os.system('clear')
+
+#--1--
+
+path=os.path.dirname(__file__)
+f=open(f'{path}{os.sep}src{os.sep}tavirathu13.txt')
+
+met=[]
+
+for l in f:
+    telepules=l.strip().split()[0]
+    hm=l.strip().split()[1][0:4:1]
+    i=l.strip().split()[2][0:3:1]
+    e=l.strip().split()[2][3:5:1]
+    c=int(l.strip().split()[3])
+    met.append([telepules,hm,i,e,c])
+        
+f.close()
+
+print(met[0])
+
+#--2--
+print('--2--')
+
+i=len(met)-1
+t="SM"
+
+while i>=0 or met[i][0]!=t:
+    i=i-1
+print(f'Az alabbi telepulesrol: {met[i][0]} utoljara {met[i][1]}:{met[i][2]}-kor erkezett adat.')
+
+#--3--
+print('--3--')
+min=int(met[0][4])
+max=int(met[0][4])
+maxindex=0
+minindex=0
+
+
+for i in range(len(met)):
+    if int(met[i][4])>max:        
+        max=int(met[i][4])
+        maxindex=i
+    if int(met[i][4])<min:        
+        min=int(met[i][4])
+        minindex=i
+    
+
+
+print(f'legalacsonyabb homerseklet: {met[minindex][0]} {met[minindex][1][0:2:1]}:{met[minindex][1][2:4:1]} {met[minindex][4]}')
+print(f'legmagasabb homerseklet: {met[maxindex][0]} {met[maxindex][1][0:2:1]}:{met[maxindex][1][2:4:1]} {met[maxindex][4]}')
+
+#--4--
+print('--4--')
+szelcsend=[]
+
+for i in range(len(met)):
+    if met[i][2]=="000" and met[i][3]=="00":
+        szelcsend.append(met[i])
+
+if len(szelcsend)==0:
+    print("Nem volt szelcsend a meresek idejen.")
+else:
+    print('Az alabbi helyeken volt szelcsend:')
+    for i in range(len(szelcsend)):
+        print(f'{szelcsend[i][0]} {szelcsend[i][1][0:2:1]}:{szelcsend[i][1][2:4:1]}')
