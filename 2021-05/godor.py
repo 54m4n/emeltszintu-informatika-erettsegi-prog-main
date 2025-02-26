b98346141eac32cd863b3079fa09586a9b746404
54m4n <pflum.tamas@gmail.com>
2025-02-26 18:48:45 +0100

    0

--- 2021-05/godor.py
+++ 2021-05/godor.py
@@ -21,7 +21,13 @@ print(f'az adatforras {len(godor)} adatot tartalmaz')
 print('--2--')
 #x=int(input(f'bazd ide a tavolsag erteket (max {len(godor)} lehet a szam):'))
 x=9
-print(f'a godor a megadott tavolsagerteken ({x} meter) pontosan {godor[x]} m mely')
+vangodor=False
+if godor[x]!=0:
+    print(f'a godor a megadott tavolsagerteken ({x} meter) pontosan {godor[x]} m mely')
+    vangodor=True
+else:
+    print(f'itt bizony nincs godor ({x} meteren)')
+
 
 #--3--
 print('--3--')
@@ -71,6 +77,39 @@ while godor[veg]!=0:
 print(f'a godor kiindulopontja: {x}m, kezdete: {kezdet}m, vege {veg}m')
 
 print('--6b--')
+maxgodor=[]
+#if vangodor!=False:
+
+for i in range(kezdet,veg):
+    maxgodor.append(godor[i])
+
+print(maxgodor)
+
+maxgodor=[1,2,3,4,5,5,5,2,1]
+print(maxgodor)
+melyul=True
+
+i=0
+while i<=len(maxgodor) and maxgodor[i]!=max(maxgodor) and melyul==True:
+    if maxgodor[i]>maxgodor[i+1]:
+        melyul=False
+    else:
+        print(maxgodor[i])
+        i=i+1
+    if maxgodor[i]==max(maxgodor):
+        while i!=len(maxgodor) and melyul==True:
+            if maxgodor[i-1]>maxgodor[i]:
+                melyul=False
+            else:
+                print(maxgodor[i])
+            if i+1!=len(maxgodor):
+                i=i+1
 
 
 
+
+print()
+print(melyul)
+
+# 1,2,3,4,3,2,1
+# 1,2,2,4,3,4,1
