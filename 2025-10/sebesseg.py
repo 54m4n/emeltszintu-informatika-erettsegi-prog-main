8bd23d5d9834caf1eb2f099b2b32ed7838f273ec
54m4n <pflum.tamas@gmail.com>
2026-02-04 08:59:17 -0500

    0

--- 2025-10/sebesseg.py
+++ 2025-10/sebesseg.py
@@ -9,4 +9,36 @@ else:
 #--1--
 print('--1--')
 
-print(os.path.dirname(__file__))
+path=os.path.dirname(__file__)
+
+f=open(f'{path}{os.sep}src{os.sep}ut.txt')
+
+ut=[]
+c=0
+for i in f:
+    if c==0:
+        sumroad=int(i)
+    else:
+        a=int(i.strip().split()[0])
+        try:
+            b=int(i.strip().split()[1])
+        except:
+            b=i.strip().split()[1]        
+        ut.extend([[a,b]])
+    c=c+1
+f.close()
+
+#--2--
+print('--2--')
+
+for i in range(len(ut)):
+    if len(str(ut[i][1]))>=4:
+        print(ut[i][1])
+        ut[i].append(90)
+    
+    
+#--3--
+print('--3--')
+
+
+print(ut)
